2e7917c05a8fab057b9e73723578ade8eb2d966a
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-08-16 20:12:43 -0700

    Adding a paragraph

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+print("There once was a man named George, ")
+print("he was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
